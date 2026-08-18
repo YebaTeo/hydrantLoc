8142cbcc8610854a3b193a13a5104fba371d463a
yebateo <yebateo24@gmail.com>
2026-08-18 10:35:05 +0700

    Initial Commit

---
+++ hydrant/hydrantApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydrantApp.swift
+//  hydrant
+//
+//  Created by Yeba Teo on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct hydrantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
