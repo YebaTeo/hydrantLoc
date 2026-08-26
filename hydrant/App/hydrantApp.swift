cb063a8d5ac8b13b7dd5daf0faede9f009e378e9
daffabur <daffaburane123@gmail.com>
2026-08-26 09:19:36 +0700

    Onboarding

--- hydrant/App/hydrantApp.swift
+++ hydrant/App/hydrantApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct hydrantApp: App {
+
     var body: some Scene {
+
         WindowGroup {
-            HomeView()
+            RootView()
         }
     }
 }
---
+++ hydrant/data/OnboardingData.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingData.swift
+//  hydrant
+//
+//  Created by Daffa Burane Nugraha on 26/08/26.
+//
+
+
+
+import Foundation
+
+struct OnboardingData {
+
+    static let roles: [UserRole] = UserRole.allCases
+
+    static let taskForces: [TaskForce] = TaskForce.allCases
+}
